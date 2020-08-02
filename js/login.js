ccbf2066e6e92dbae5f8f436da7b39aa1a03011e
JAgamez21 <66244802+JAgamez21@users.noreply.github.com>
2020-08-01 19:17:04 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,51 @@
+const usuarios = [
+  {
+    "user": "Julian",
+    "password": "0421",
+    "tipo": "Administrador"
+  },
+  {
+    "user": "Diego",
+    "password": "2404",
+    "tipo": "Odontologo"
+  },
+  {
+    "user": "Jesus",
+    "password": "0000",
+    "tipo": "cliente"
+  }
+]
+
+eventListeners();
+
+function eventListeners() {
+  //Cuando se envia el formulario
+  document.getElementById('formlogin').addEventListener('submit', formlogin);
+}
+
+function formlogin(e) {
+  //Prevenimos el envio del formulario
+  e.preventDefault()
+
+  const user = document.getElementById('user').value
+  const password = document.getElementById('password').value
+
+  let login = false
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].user === user && usuarios[i].password === password) {
+      setUserSessionStorage(usuarios[i])
+      login = true
+      break;
+    }
+  }
+
+  if (login) {
+    location.href = 'PAGWEBJS/../index.html'
+  } else {
+    alert('User/password incorrect')
+  }
+}
+
+function setUserSessionStorage(user) {
+  sessionStorage.setItem('user', JSON.stringify(user))
+}
